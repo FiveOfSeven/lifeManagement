9374f59057a108563435967011e5fc43fb2b10f9
Carl Shurtliff <cphilshurtliff@gmail.com>
2018-12-04 20:25:09 -0700

    was able to get input from user while the program can break out

--- lifeManagement.py
+++ lifeManagement.py
@@ -4,8 +4,23 @@ stdscr = curses.initscr()
 curses.noecho()
 curses.cbreak()
 stdscr.keypad(True)
+stdscr.nodelay(1)
 
 time.sleep(1)
+
+curses.start_color()
+curses.halfdelay(100)
+ch = stdscr.getch()
+if (ch == 113):
+    stdscr.addstr(0, 0, "you made it hahahaha... Current mode: Typing mode", curses.A_REVERSE)
+    stdscr.refresh()
+
+time.sleep(2)
+
+stdscr.addstr(0, 0, "Current mode: Typing mode", curses.A_REVERSE)
+stdscr.refresh()
+
+time.sleep(4)
 
 curses.nocbreak()
 stdscr.keypad(False)
@@ -16,5 +31,6 @@ time.sleep(1)
 
 #while (char != 'q'):
 print(time.time())
+print(ch)
 
 

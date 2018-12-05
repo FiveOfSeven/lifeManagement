1834013b60e34ba932e3b9140bb6ea16adf297e4
Carl Shurtliff <cphilshurtliff@gmail.com>
2018-12-04 21:31:04 -0700

    timer with quiting on q is working

--- lifeManagement.py
+++ lifeManagement.py
@@ -6,31 +6,43 @@ curses.cbreak()
 stdscr.keypad(True)
 stdscr.nodelay(1)
 
-time.sleep(1)
+ch = 0
+counter = 0
+blankString = "                                                 "
+
+while (ch != 113):
+    curses.halfdelay(10)
+    ch = stdscr.getch()
+    #currentTime = time.time()
+    currentTime = counter
+    hours = currentTime // 3600
+    leftoverHours = currentTime % 3600
+    minutes = leftoverHours // 60
+    seconds = leftoverHours % 60
+    hours = "  Hours: " + str(hours)
+    minutes = "Minutes: " + str(minutes)
+    seconds = "Seconds: " + str(seconds)
+    stdscr.addstr(0, 0, blankString)
+    stdscr.addstr(1, 0, blankString)
+    stdscr.addstr(2, 0, blankString)
+    stdscr.addstr(0, 0, hours, curses.A_REVERSE)
+    stdscr.addstr(1, 0, minutes, curses.A_REVERSE)
+    stdscr.addstr(2, 0, seconds, curses.A_REVERSE)
+    stdscr.addstr(4, 0, str(counter), curses.A_REVERSE)
+    stdscr.refresh()
+    counter = counter + 1
+    
+
 
 curses.start_color()
-curses.halfdelay(100)
-ch = stdscr.getch()
-if (ch == 113):
-    stdscr.addstr(0, 0, "you made it hahahaha... Current mode: Typing mode", curses.A_REVERSE)
-    stdscr.refresh()
-
-time.sleep(2)
-
-stdscr.addstr(0, 0, "Current mode: Typing mode", curses.A_REVERSE)
-stdscr.refresh()
-
-time.sleep(4)
-
 curses.nocbreak()
 stdscr.keypad(False)
 curses.echo()
 curses.endwin()
 
-time.sleep(1)
 
-#while (char != 'q'):
 print(time.time())
+print(seconds)
 print(ch)
 
 
